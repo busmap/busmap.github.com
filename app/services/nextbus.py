066577a1c498a4357ef0f15b8930bd173a08e3ae
Glen Robertson <glen@lyft.com>
2018-02-11 16:41:07 -0800

    Simplify

--- app/services/nextbus.py
+++ app/services/nextbus.py
@@ -72,47 +72,11 @@ def get_routes_by_nearby_stops(
     nearby_stop_ids = [stop['tag'] for stop in nearby_stops]
 
     stop_route_ids = []
+
+    nearby_route_id_stop_ids = set()
     for stop_id in nearby_stop_ids:
         for route_id in stops[stop_id]['route_ids']:
-            stop_route_ids.append('|'.join([route_id, stop_id]))
-
-    prediction_resp = get_predictions_by_stop_ids(stop_route_ids)
-
-    routes_by_stop = {}
-    for prediction in prediction_resp['predictions']:
-        if 'direction' not in prediction:
-            continue
-        stop_tag = prediction['stopTag']
-        route_tag = prediction['routeTag']
-
-        if stop_tag not in routes_by_stop:
-            routes_by_stop[stop_tag] = []
-
-        if isinstance(prediction['direction'], dict):
-            directions = [prediction['direction']]
-        else:
-            directions = prediction['direction']
-        for direction in directions:
-            if isinstance(direction['prediction'], dict):
-                direction_predictions = [direction['prediction']]
-            else:
-                direction_predictions = direction['prediction']
-
-            for direction_prediction in direction_predictions:
-                dir_tag = direction_prediction['dirTag']
-                routes_by_stop[stop_tag].append((route_tag, dir_tag))
-
-    nearby_route_ids = []
-    nearby_route_id_stop_ids = []
-
-    for stop_id in nearby_stop_ids:
-        stop_routes = routes_by_stop.get(stop_id, [])
-        for route_id, dir_id in stop_routes:
-            if route_id not in nearby_route_ids:
-                nearby_route_ids.append(route_id)
-                nearby_route_id_stop_ids.append((route_id, stop_id))
-            if len(nearby_route_ids) == 10:
-                break
+            nearby_route_id_stop_ids.add((route_id, stop_id))
 
     return [
         (routes[route_id], stops[stop_id])
